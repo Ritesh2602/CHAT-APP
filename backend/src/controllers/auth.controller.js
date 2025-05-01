6e09614a60a68d68aca81fe833a3186de2f0e4cc
Ritesh Tahilramani <riteshtahilramani1@gmail.com>
2025-05-02 03:02:48 +0530

    Created some routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (res, req) => {
+  res.send("Signup route");
+};
+
+export const login = (res, req) => {
+  res.send("Login route");
+};
+
+export const logout = (res, req) => {
+  res.send("Logout route");
+};
