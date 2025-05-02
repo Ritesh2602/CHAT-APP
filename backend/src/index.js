2eb730226382893e43ecf7d660f97940c287ef11
Ritesh Tahilramani <riteshtahilramani1@gmail.com>
2025-05-02 19:05:27 +0530

    established connection with mongoDb using mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
-import express from "express";
+import express from "express"; //Core framework for handling HTTP requests,routing,middleware, etc.
+import dotenv from "dotenv"; //Loads the dotenv package.
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config(); //Loads .env file contents into process.env.
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
 });
